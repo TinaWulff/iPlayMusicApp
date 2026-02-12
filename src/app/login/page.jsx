b42b3107c4e1f679c8e9c8383b789aa5d6609776
Tina Wulff <tina@tinawulff.dk>
2026-02-12 16:10:26 +0100

    rettet en fejl i callbacket/login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,20 +2,10 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
 export default function LoginPage() {
+	const scope = encodeURIComponent("playlist-read-private user-read-recently-played user-top-read");
+	const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&show_dialog=true&scope=${scope}`;
+	
 	return (
-		<a href={`https://accounts.spotify.com/authorize?
-			response_type=code
-			&client_id=${CLIENT_ID}
-			&redirect_uri=${REDIRECT_URI}
-			&show_dialog=true
-			&scope=playlist-read-private 
-			user-read-recently-played user-top-read`}>To access please Log in with Spotify</a>
+		<a href={authUrl}>To access please Log in with Spotify</a>
 	);
 }
-
-// {`https://accounts.spotify.com/authorize?
-// 			response_type=code
-// 			&client_id=${CLIENT_ID}
-// 			&redirect_uri=${REDIRECT_URI}
-// 			&show_dialog=true
-// 			&scope=playlist-read-private`}
